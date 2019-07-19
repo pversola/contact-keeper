d98ccf4e5d44ce74b45a96b3ffc5f9979517a5c8
unknown <pversola@gmail.com>
2019-07-19 15:22:27 +0700

    Removed Initial

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use('/api/contacts', require('./routes/contacts'));
 // Server sttic asset in production
 if(process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use();
+    app.use(express.static('client/build'));
     app.get('*', (req, res) => 
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
 }
